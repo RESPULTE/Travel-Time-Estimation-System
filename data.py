e7c1cd0d614a78fde7c7bbe6ddfc978945947fe9
VectorLim <yeapzing@gmail.com>
2023-07-26 11:39:55 +0800

    reworked the handling_data_manipulation() function and beautified the printing format

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractclassmethod
-from .utils import prompt_input
+from utils import *
+from typing import List
+
+DATA_MANIPULATION_MENU = "1: add    2: edit    3: delete    4. exit"
 
 
 class Data(ABC):
@@ -35,6 +38,73 @@ class Data(ABC):
         ...
 
     @classmethod
+    def handle_data_manipulation(cls, filename: str):
+        """
+        This function is responsible for all the add/edit/delete that can be done
+        to the location's data or the transport's data. It does the selected command
+        and then updates the file that stores the data accordingly
+        """
+        while True:
+            cls.print_database()
+
+            cmd = prompt_input(DATA_MANIPULATION_MENU, lambda x: x in ["1", "2", "3", "4", "5"])
+
+            # creates a new data, write it to file and then add it to the database
+            if cmd == "1":
+                new_data = cls.new()
+                with open(filename, "a") as f:
+                    f.write(new_data.to_file())
+                cls.database.append(new_data)
+
+            elif cmd == "2":
+                if len(cls.database) == 0:
+                    print("nothing to edit")
+                    continue
+
+                # get the data to edit using the index given by the user
+                index = cls.get_valid_index(msg="element to edit:   ")
+                data = cls.database[index - 1]
+
+                # prompt the user to confirm the decision to change the selected data
+                if not prompt_user_to_confirm("edit the selected data:  "):
+                    continue
+
+                data.update()
+
+                old_loc_data = data.to_file()
+                with open(filename, "r+") as f:
+                    loc_data = f.read()
+                    # look for the old data in the file and replace it with the new data
+                    updated_loc_data = loc_data.replace(old_loc_data, data.to_file())
+
+                    # This reset's the file reader's pointer so that it starts from the beginning of
+                    # the file instead of where it had left off
+                    f.seek(0)
+                    f.write(updated_loc_data)
+
+            elif cmd == "3":
+                if len(cls.database) == 0:
+                    print("nothing to delete")
+                    continue
+
+                # prompt and confirms with the user about the index of the data to delete
+                index = cls.get_valid_index(msg="data to delete:    ")
+                if not prompt_user_to_confirm("delete the selected data:    "):
+                    return
+
+                # remove the deleted data from the database
+                data = cls.database.pop(index - 1)
+
+                with open(filename, "r+") as f:
+                    loc_data = f.read()
+                    # find the deleted data in the file and remove it
+                    updated_loc_data = loc_data.replace(data.to_file(), "")
+                    f.seek(0)
+                    f.write(updated_loc_data)
+            elif cmd == "4":
+                break
+
+    @classmethod
     def get_new_name(cls, skip=False):
         """
         This function prompts the user to give a valid name for the new data that does not exist in the database
@@ -43,8 +113,8 @@ class Data(ABC):
         # cls.__name__.removesuffix('Data') is just to get the class's name, which can either be "Location" or "Transport"
         # This is used for clarification when outputting the message to the user
         return prompt_input(
-            f"Please input new {cls.__name__.removesuffix('Data')}'s name",
-            lambda x: x not in [i.name for i in cls.database],
+            f"Please input new {cls.__name__.removesuffix('Data')}'s name:  ",
+            lambda x: x not in [i.name for i in cls.database] and x != "",
             skip=skip,
         )
 
@@ -58,9 +128,9 @@ class Data(ABC):
 
 
 class TransportData(Data):
-    database = []
+    database: List["TransportData"] = []
 
-    def __init__(self, name, speed):
+    def __init__(self, name: str, speed: float):
         self.name = name
         self.speed = float(speed)
 
@@ -70,8 +140,15 @@ class TransportData(Data):
 
     @classmethod
     def print_database(cls):
+        table_len = 5 + 5 + 50 + 15
+        print("\n" + "-" * table_len)
+        print(f"|{''.center(5)}|{'Name of Transport'.center(50)}|{'Speed'.center(15)}|")
+        print("-" * table_len)
+
         for i, vtrans in enumerate(TransportData.database, 1):
-            print(f"{i}. {vtrans.name}  |  speed:{vtrans.speed}")
+            print(f"|{(str(i) + '.').center(5)}|{vtrans.name.center(50)}|{(str(vtrans.speed) + 'KM/h').center(15)}|")
+
+        print("-" * table_len)
 
     def to_file(self):
         return f"{self.name},{self.speed}\n"
@@ -82,13 +159,13 @@ class TransportData(Data):
 
     @staticmethod
     def get_new_speed(skip=False):
-        return prompt_input("Please input transport's speed", lambda x: x > 0, func=float, skip=skip)
+        return prompt_input("Please input transport's speed:    ", lambda x: x > 0, func=float, skip=skip)
 
 
 class LocationData(Data):
-    database = []
+    database: List["LocationData"] = []
 
-    def __init__(self, name, latitude, longitude):
+    def __init__(self, name: str, latitude, longitude):
         self.name = name
         self.latitude = float(latitude)
         self.longitude = float(longitude)
@@ -103,8 +180,16 @@ class LocationData(Data):
 
     @classmethod
     def print_database(cls):
+        table_len = 5 + 5 + 50 + 15 + 15
+        print("\n" + "-" * table_len)
+        print(f"|{''.center(5)}|{'Name of Location'.center(50)}|{'Latitude'.center(15)}|{'Longitude'.center(15)}|")
+        print("-" * table_len)
+
         for i, vloc in enumerate(LocationData.database, 1):
-            print(f"{i}. {vloc.name}  |  latitude:{vloc.latitude}  |  longitude: {vloc.longitude}")
+            print(
+                f"|{(str(i) + '.').center(5)}|{vloc.name.center(50)}|{str(vloc.latitude).center(15)}|{str(vloc.longitude).center(15)}|"
+            )
+        print("-" * table_len)
 
     def to_file(self):
         return f"{self.name},{self.latitude},{self.longitude}\n"
@@ -116,4 +201,6 @@ class LocationData(Data):
 
     @staticmethod
     def get_new_coordinate(coodinate_type: str, skip=False):
-        return prompt_input(f"Please input location's {coodinate_type}", lambda x: 0 <= x <= 180, func=float, skip=skip)
+        return prompt_input(
+            f"Please input location's {coodinate_type}: ", lambda x: 0 <= x <= 180, func=float, skip=skip
+        )
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from math import radians, cos, sin, asin, sqrt
 
-from .utils import prompt_user_to_confirm
-from .data import *
+from utils import prompt_user_to_confirm
+from data import *
 
 WELCOME_MESSAGE = """
 -----------------------------------------
  Welcome to Personal Travel Assistance !
 -----------------------------------------
 """
-DATA_MANIPULATION_MENU = "1: add    2: edit    3: delete    4. exit"
+
 LOCATIONDATA_FILENAME = "LocationData.txt"
 TRANSPORTDATA_FILENAME = "TransportData.txt"
 
@@ -40,93 +40,46 @@ def run_simulation():
     the time to get there with the selected transport
     """
     while True:
+        # displaying the table of all locations for the user to choose
+        table_len = 3 + 5 + 50
+        print("-" * table_len)
+        print(f"|{'no.'.center(5)}|{'Location'.center(50)}|")
+        print("-" * table_len)
         for i, vloc in enumerate(LocationData.database, 1):
-            print(f"{i}. {vloc.name}")
+            print(f"|{(str(i) + '.').center(5)}|{vloc.name.center(50)}|")
+            print("-" * table_len)
+
+        # prompting the user to choose the starting and ending location
         start_loc: LocationData = LocationData.database[LocationData.get_valid_index(msg="starting location") - 1]
         dest_loc: LocationData = LocationData.database[LocationData.get_valid_index(msg="destination") - 1]
 
+        # displaying the table of all transport for the user to choose
+        table_len = 4 + 5 + 50 + 15
+        print("-" * table_len)
+        print(f"|{'no.'.center(5)}|{'Transport'.center(50)}|{'Speed (KM/h)'.center(15)}|")
+        print("-" * table_len)
         for i, vtrans in enumerate(TransportData.database, 1):
-            print(f"{i}. {vtrans.name}, {vtrans.speed}")
+            print(f"|{(str(i) + '.').center(5)}|{str(vtrans.name).center(50)}|{str(vtrans.speed).center(15)}|")
+        print("-" * table_len)
+
+        # prompting the user to choose the transport
         transport: TransportData = TransportData.database[
             TransportData.get_valid_index(msg="transport of your choice") - 1
         ]
 
         distance = calculate_distance(start_loc, dest_loc)
-        print(f"distance: {distance}")
         time = distance / transport.speed
-        print(f"estimated time: {time}")
+
+        # displaying the result of calculation
+        print("\n")
+        print("-" * 40)
+        print(f"distance        : {round(distance, 2)}  KM")
+        print(f"estimated time  : {round(time, 2)}  hours")
+        print("-" * 40)
+        print("\n")
 
         if prompt_user_to_confirm("exit the simulation"):
             break
-
-
-def handle_data_manipulation(filename: str, data_type: LocationData | TransportData):
-    """
-    This function is responsible for all the add/edit/delete that can be done
-    to the location's data or the transport's data. It does the selected command
-    and then updates the file that stores the data accordingly
-    """
-    while True:
-        data_type.print_database()
-
-        cmd = prompt_input(DATA_MANIPULATION_MENU, lambda x: x in ["1", "2", "3", "4", "5"])
-
-        # creates a new data, write it to file and then add it to the database
-        if cmd == "1":
-            new_data = data_type.new()
-            with open(filename, "a") as f:
-                f.write(new_data.to_file())
-            data_type.database.append(new_data)
-
-        elif cmd == "2":
-            if len(data_type.database) == 0:
-                print("nothing to edit")
-                continue
-
-            # get the data to edit using the index given by the user
-            index = data_type.get_valid_index(msg="element to edit")
-            data = data_type.database[index - 1]
-
-            # prompt the user to confirm the decision to change the selected data
-            if not prompt_user_to_confirm("edit the selected data"):
-                continue
-
-            data.update()
-
-            old_loc_data = data.to_file()
-            with open(filename, "r+") as f:
-                loc_data = f.read()
-                # look for the old data in the file and replace it with the new data
-                updated_loc_data = loc_data.replace(old_loc_data, data.to_file())
-
-                # This reset's the file reader's pointer so that it starts from the beginning of
-                # the file instead of where it had left off
-                f.seek(0)
-                f.write(updated_loc_data)
-
-        elif cmd == "3":
-            if len(data_type.database) == 0:
-                print("nothing to delete")
-                continue
-
-            # prompt and confirms with the user about the index of the data to delete
-            index = data_type.get_valid_index(msg="data to delete")
-            if not prompt_user_to_confirm("delete the selected data"):
-                return
-
-            # remove the deleted data from the database
-            data = data_type.database.pop(index - 1)
-
-            with open(filename, "r+") as f:
-                loc_data = f.read()
-                # find the deleted data in the file and remove it
-                updated_loc_data = loc_data.replace(data.to_file(), "")
-                f.seek(0)
-                f.write(updated_loc_data)
-        elif cmd == "4":
-            break
-
-    ...
 
 
 def main():
@@ -141,7 +94,7 @@ def main():
         print(WELCOME_MESSAGE)
 
         cmd = prompt_input(
-            "1: start simulation   2: location    3. Transport    4. Exit", lambda x: x in ["1", "2", "3", "4"]
+            "1: start simulation   2: location    3. Transport    4. Exit\n", lambda x: x in ["1", "2", "3", "4"]
         )
 
         if cmd == "1":
@@ -155,9 +108,9 @@ def main():
 
             run_simulation()
         elif cmd == "2":
-            handle_data_manipulation(LOCATIONDATA_FILENAME, LocationData)
+            LocationData.handle_data_manipulation(LOCATIONDATA_FILENAME)
         elif cmd == "3":
-            handle_data_manipulation(TRANSPORTDATA_FILENAME, TransportData)
+            TransportData.handle_data_manipulation(TRANSPORTDATA_FILENAME)
         else:
             break
 
